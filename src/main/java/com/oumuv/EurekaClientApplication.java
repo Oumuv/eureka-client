30ba548ead95ae83eb8e4efb22ec5d907032f2f2
Oumuv <802711luo>
2018-03-15 17:50:24 +0800

    .

--- src/main/java/com/oumuv/EurekaClientApplication.java
+++ src/main/java/com/oumuv/EurekaClientApplication.java
@@ -1,16 +1,22 @@
 package com.oumuv;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient//注解表明启动的是一个eurekaclient
 @RestController
+@EnableHystrix//注解开启Hystrix
+@EnableHystrixDashboard//开启hystrixDashboard
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
@@ -20,7 +26,8 @@ public class EurekaClientApplication {
 	@Value("${server.port}")
 	String port;
 	@RequestMapping("/hi")
-	public String home(@RequestParam String name) {
-		return "hi "+name+",i am from port:" +port;
+	@HystrixCommand
+	public String home(String name) {
+		return "hi!"+name+",i am from port:" +port;
 	}
 }
